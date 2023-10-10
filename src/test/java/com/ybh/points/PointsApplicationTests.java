df0b52dd079888f333b478b86b4c24a0c977a655
ybh89 <akass191@gmail.com>
2023-10-10 14:05:46 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/ybh/points/PointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybh.points;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
